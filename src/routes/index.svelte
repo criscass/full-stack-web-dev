b18c86c90b07806ecbbb1689b31eb2c63ade44ab
criscass <71939926+criscass@users.noreply.github.com>
2022-07-19 12:38:47 +0200

    Apply CSS styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
   import TodoItem from "../lib/todo-item.svelte";
+  import "../app.css";
 
   const pageTitle = "Todos";
 </script>
@@ -7,17 +8,49 @@
 <svelte:head>
   <title>{pageTitle}</title>
 </svelte:head>
+<div class="todos">
+  <h1>{pageTitle}</h1>
 
-<h1>{pageTitle}</h1>
+  <form action="" method="" class="new">
+    <input
+      type="text"
+      name="text"
+      id=""
+      aria-label="Add a Todo"
+      placeholder="+ type to add a Todo"
+    />
+  </form>
+  <TodoItem />
+  <TodoItem />
+  <TodoItem />
+</div>
 
-<form action="" method="">
-  <input
-    type="text"
-    name="text"
-    id=""
-    aria-label="Add a Todo"
-    placeholder="+ type to add a Todo"
-  />
-</form>
+<style>
+  .todos {
+    width: 100%;
+    max-width: 42rem;
+    margin: 4rem auto 0 auto;
+  }
 
-<TodoItem />
+  .new {
+    margin: 0 0 0.5rem 0;
+  }
+  .new input {
+    font-size: 28px;
+    width: 100%;
+    padding: 0.5em 1em 0.3em 1em;
+    box-sizing: border-box;
+    background: rgba(255, 255, 255, 0.05);
+    border-radius: 8px;
+    text-align: center;
+  }
+  .todos :global(input) {
+    border: 1px solid transparent;
+  }
+
+  .todos :global(input:focus-visible) {
+    box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.1);
+    border: 1px solid #ff3e00 !important;
+    outline: none;
+  }
+</style>
